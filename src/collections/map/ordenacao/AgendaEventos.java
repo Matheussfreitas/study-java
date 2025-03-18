11e4f1d11648fc2d3b7c87cf296c53a0b9d1e606
matheus <matheus@bolt360.com.br>
2025-03-18 12:44:57 -0300

    finalizando Map

--- src/collections/map/ordenacao/AgendaEventos.java
+++ src/collections/map/ordenacao/AgendaEventos.java
@@ -20,4 +20,30 @@ public class AgendaEventos {
         Map<LocalDate, Evento> eventosOrdenados = new TreeMap<>(eventosAgendados);
         System.out.println(eventosOrdenados);
     }
+
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosAgendados);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            if (entry.getKey().equals(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("Próximo evento: " + proximaData + " - " + proximoEvento);
+                break;
+            } else {
+                System.out.println("Não há eventos agendados.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agenda = new AgendaEventos();
+        agenda.adicionarEvento(LocalDate.of(2021, 10, 10), "Show de Rock", "Banda X");
+        agenda.adicionarEvento(LocalDate.of(2021, 10, 15), "Show de MPB", "Cantor Y");
+        agenda.adicionarEvento(LocalDate.of(2021, 10, 20), "Show de Samba", "Grupo Z");
+        agenda.exibirEventos();
+        agenda.obterProximoEvento();
+    }
 }
